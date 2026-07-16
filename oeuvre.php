420182154f955c584632f09a04a4cecece9e251f
Michael Laau <michael.laau2401@gmail.com>
2026-07-16 19:44:09 +0200

    feat: display single oeuvre from database, redirect on invalid id

--- oeuvre.php
+++ oeuvre.php
@@ -1,36 +1,33 @@
 <?php
+    require 'bdd.php';
     require 'header.php';
-    require 'oeuvres.php';
 
-    // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
-    if(empty($_GET['id'])) {
+    $id = intval($_GET['id']);
+
+    if(empty($id)){
         header('Location: index.php');
+        exit;
     }
 
-    $oeuvre = null;
-
-    // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
-        // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
-            break; // On stoppe le foreach si on a trouvé l'oeuvre
-        }
-    }
-
-    // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
-    if(is_null($oeuvre)) {
+    $bdd = connexion();
+    $sqlQuery = 'SELECT * FROM oeuvres WHERE id = ?';
+    $statement = $bdd->prepare($sqlQuery);
+    $statement->execute([$id]);
+    $oeuvre = $statement->fetch();
+   
+    if(!$oeuvre){
         header('Location: index.php');
+        exit;
     }
 ?>
 
 <article id="detail-oeuvre">
     <div id="img-oeuvre">
-        <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
+        <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['title'] ?>">
     </div>
     <div id="contenu-oeuvre">
-        <h1><?= $oeuvre['titre'] ?></h1>
-        <p class="description"><?= $oeuvre['artiste'] ?></p>
+        <h1><?= $oeuvre['title'] ?></h1>
+        <p class="description"><?= $oeuvre['artist'] ?></p>
         <p class="description-complete">
              <?= $oeuvre['description'] ?>
         </p>
